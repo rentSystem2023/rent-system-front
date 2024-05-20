5a7205d0e8902b2b8bf6a3dae31e34a9b6019531
qm0u0mp <99dngksmf@gmail.com>
2024-05-20 15:28:40 +0900

    constant 작성

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,129 +1,126 @@
+// description : URL PATH
+export const MAIN_PATH = 'main';
+export const SNS_PATH = '/sns/:accessToken/:expires';
+
+export const AUTH_PATH = '/authentication';
+export const SIGN_IN_PATH = 'sign-in';
+export const SIGN_UP_PATH = 'sign-up';
+
+export const USER_PATH = '/user';
+export const USER_INFO_PATH = 'information';
+export const USER_UPDATE_PATH = 'modify';
+export const USER_LIST_PATH = 'list';
+export const USER_DETAIL_PATH = ':userId';
+
+export const RESERVATION_PATH = '/reservation';
+export const RESERVATION_MY_LIST_PATH = 'mylist';
+export const RESERVATION_MY_DETAIL_PATH = ':reservationCode';
+export const RESERVATION_LIST_PATH = 'list';
+
+export const NOTICE_PATH = '/notice';
+export const NOTICE_WRITE_PATH = 'regist';
+export const NOTICE_DETAIL_PATH = ':registNumber';
+export const NOTICE_UPDATE_PATH = 'update/:registNumber';
+
+export const QNA_PATH = '/qna';
+export const QNA_LIST_PATH = 'list';
+export const QNA_WRITE_PATH = 'regist';
+export const QNA_DETAIL_PATH = ':receptionNumber';
+export const QNA_UPDATE_PATH = 'update/:receptionNumber';
+export const QNA_MY_LIST_PATH = 'myList';
+
+//description : Navigation 절대 URL PATH
+export const MAIN_ABSOLUTE_PATH = MAIN_PATH;
+
+export const SIGN_IN_ABSOLUTE_PATH = `${AUTH_PATH}/${SIGN_IN_PATH}`;
+export const SIGN_UP_ABSOLUTE_PATH = `${AUTH_PATH}/${SIGN_UP_PATH}`;
+
+export const USER_INFO_ABSOLUTE_PATH = `${USER_PATH}/${USER_INFO_PATH}`;
+export const USER_INFO_MODIFY_ABSOLUTE_PATH = `${USER_INFO_ABSOLUTE_PATH}/${USER_UPDATE_PATH}`;
+export const USER_LIST_ABSOLUTE_PATH = `${USER_PATH}/${USER_LIST_PATH}`;
+export const USER_DETAIL_ABSOLUTE_PATH = (userId : number) => `${USER_LIST_ABSOLUTE_PATH}/${userId}`;
+
+export const RESERVATION_MY_LIST_ABSOLUTE_PATH = `${RESERVATION_PATH}/${RESERVATION_MY_LIST_PATH}`;
+export const RESERVATION_MY_DETAIL_ABSOLUTE_PATH = (reservationCode : string) => `${RESERVATION_MY_LIST_ABSOLUTE_PATH}/${reservationCode}`;
+export const RESERVATION_LIST_ABSOLUTE_PATH = `${RESERVATION_PATH}/${RESERVATION_LIST_PATH}`;
+
+export const NOTICE_WRITE_ABSOLUTE_PATH = `${NOTICE_PATH}/${NOTICE_WRITE_PATH}`;
+export const NOTICE_ABSOLUTE_DETAIL_PATH = (registNumber : number) => `${NOTICE_WRITE_ABSOLUTE_PATH}/${registNumber}`;
+export const NOTICE_ABSOLUTE_UPDATE_PATH = (registNumber : number) => `${NOTICE_WRITE_ABSOLUTE_PATH}/update/${registNumber}`;
+
+export const QNA_LIST_ABSOLUTE_PATH = `${QNA_PATH}/${QNA_LIST_PATH}`;
+export const QNA_WRITE_ABSOLUTE_PATH = `${QNA_PATH}/${QNA_WRITE_PATH}`;
+export const QNA_DETAIL_ABSOLUTE_PATH = (receptionNumber : number) => `${QNA_LIST_ABSOLUTE_PATH}/${receptionNumber}`
+export const QNA_UPDATE_ABSOLUTE_PATH = (receptionNumber : number) => `${QNA_LIST_ABSOLUTE_PATH}/update/${receptionNumber}`;
+
 // description : 도메인 URL
-export const SERVER_DOMAIN_DOMAIN = 'http://localhost:4000';
+export const SERVER_DOMAIN_URL = 'http://localhost:4000';
 
 // description: API URL PATH 
-export const SERVER_API_URL = `${SERVER_DOMAIN_DOMAIN}/api/rentcar/auth`;
+export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/api/rentcar`;
+
+// description: AUTH MODULE
 export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
 
-export const SIGN_IN_REQUEST_URL = `${SERVER_API_URL}/sign-in`;
-export const ID_CHECK_REQUEST_URL = `${SERVER_API_URL}/id-check`;
-export const EMAIL_AUTH_REQUEST_URL = `${SERVER_API_URL}/email-auth`;
-export const EMAIL_AUTH_CHECK_REQUEST_URL = `${SERVER_API_URL}/email-auth-check`;
-export const SIGN_UP_REQUEST_URL = `${SERVER_API_URL}/sign-up`;
+export const SIGN_IN_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-in`;
+export const ID_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/id-check`;
+export const EMAIL_AUTH_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth`;
+export const EMAIL_AUTH_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth-check`;
+export const SIGN_UP_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-up`;
 
-export const GET_USER_INFO_URL = `${SERVER_API_URL}/`;
-export const GET_MY_INFO_URL = `${SERVER_API_URL}/information`;
-export const MODIFY_MY_INFO_URL = `${SERVER_API_URL}/information/modify`;
-export const DELETE_MY_ACCOUNT_URL = `${SERVER_API_URL}/information/{userId}`;
+// description: USER MODULE
+export const SERVER_USER_MODULE_URL = `${SERVER_API_URL}/user`;
 
-// description: User URL
-export const GET_USER_LIST_URL = `${SERVER_API_URL}/list`;
-export const DELETE_USER_URL = `${SERVER_API_URL}/list/{userId}`;
+export const GET_SIGN_IN_USER_REQUEST_URL = `${SERVER_USER_MODULE_URL}/`;
+export const GET_MY_INFO_URL = `${SERVER_USER_MODULE_URL}/information`;
+export const MODIFY_MY_INFO_URL = `${SERVER_USER_MODULE_URL}/information/modify`;
 
-// description: NOTICE URL
-export const GET_NOTICE_LIST_URL = `${SERVER_API_URL}/list`;
-export const SEARCH_NOTICE_LIST_URL = `${SERVER_API_URL}/list/search`;
-export const GET_NOTICE_URL = `${SERVER_API_URL}/list/{registNumber}`;
-export const INCREASE_NOTICE_VIEW_COUNT_URL = `${SERVER_API_URL}/{registNumber}/increase-view-count`;
+export const GET_USER_LIST_URL = `${SERVER_USER_MODULE_URL}/list`;
+export const GET_USER_DETAIL_URL = (userId: string) => `${SERVER_USER_MODULE_URL}/list/${userId}`;
+export const USER_LIST_SEARCH_URL = `${SERVER_USER_MODULE_URL}/liser/search`;
 
-export const POST_REGIST_NOTICE_URL = `${SERVER_API_URL}/regist`;
-export const POST_MODIFY_NOTICE_URL = `${SERVER_API_URL}/list/{registNumber}`;
-export const DELETE_NOTICE_URL = `${SERVER_API_URL}/{registNumber}`;
+// description: RESERVATION MODULE
+export const SERVER_RESERVATION_MODULE_URL = `${SERVER_API_URL}/reservation`;
 
-// description: Q&A URL
-export const GET_QNA_LIST_URL = `${SERVER_API_URL}/list`;
-export const SEARCH_QNA_LIST_URL = `${SERVER_API_URL}/list/search`;
-export const GET_QNA_URL = `${SERVER_API_URL}/list/{receptionNumber}`;
-export const WRITE_QNA_URL = `${SERVER_API_URL}/regist`;
-export const INCREASE_QNA_VIEW_COUNT_URL = `${SERVER_API_URL}/{receptionNumber}/increase-view-count`;
-export const DELETE_QNA_URL = `${SERVER_API_URL}/list/{receptionNumber}`;
-export const MODIFY_QNA_URL = `${SERVER_API_URL}/list/{receptionNumber}`;
-export const WRITE_QNA_COMMENT_URL = `${SERVER_API_URL}/{receptionNumber}/admin-comment`;
+export const GET_USER_RESERVATION_LIST_URL = `${SERVER_RESERVATION_MODULE_URL}/mylist`;
+export const GET_USER_RESERVATION_DETAIL_URL = (reservationCode : string) => `${SERVER_RESERVATION_MODULE_URL}/mylist/${reservationCode}`;
 
-// description: MYPAGE URL
+export const SEARCH_CAR_RESULT_URL = `${SERVER_RESERVATION_MODULE_URL}/search`;
+export const SEARCH_INSURANCE_RESULT_URL = (carCode : number) => `${SERVER_RESERVATION_MODULE_URL}/search/${carCode}`;
+export const SEARCH_INSURANCE_DETAIL_URL = (companyCarCode : number) => `${SERVER_RESERVATION_MODULE_URL}/search/${companyCarCode}`;
 
-export const GET_MY_QNA_LIST_URL = `${SERVER_API_URL}/mylist`;
-export const SEARCH_MY_QNA_LIST_URL = `${SERVER_API_URL}/mylist/search`;
+export const GET_RESERVATION_LIST_URL = `${SERVER_RESERVATION_MODULE_URL}/list`;
 
-// description: RESERVATION URL
-export const GET_RESERVATION_LIST_URL = `${SERVER_API_URL}/mylist`;
-export const GET_RESERVATION_DETAIL_URL = `${SERVER_API_URL}/mylist/{reservationCode}`;
-export const CANCEL_RESERVATION_URL = `${SERVER_API_URL}/mylist/{reservationCode}`;
-export const GET_POPULAR_CAR_LIST_URL = `${SERVER_API_URL}/popularcar-list`;
-export const SEARCH_CAR_RESULT_URL = `${SERVER_API_URL}/search`;
-export const SEARCH_INSURANCE_RESULT_URL = `${SERVER_API_URL}/search/{carCode}`;
-export const SEARCH_INSURANCE_DETAIL_URL = `${SERVER_API_URL}/search/{companyCarCode}`;
-export const MAKE_RESERVATION_URL = `${SERVER_API_URL}/search/{companyCarCode}`;
+// description: NOTICE MODULE
+export const SERVER_NOTICE_MODULE_URL = `${SERVER_API_URL}/notice`;
 
-// description: COMPANY URL
-export const GET_COMPANY_LIST_URL = `${SERVER_API_URL}/list`;
-export const SEARCH_COMPANY_LIST_URL = `${SERVER_API_URL}/list/search`;
-export const POST_REGISTER_COMPANY_URL = `${SERVER_API_URL}/regist`;
-export const PATCH_MODIFY_COMPANY_URL = `${SERVER_API_URL}/list/{companyCode}`;
+export const GET_NOTICE_LIST_URL = `${SERVER_NOTICE_MODULE_URL}/list`;
+export const NOTICE_LIST_SEARCH_URL = `${SERVER_NOTICE_MODULE_URL}/list/search`;
+export const GET_NOTICE_DETAIL_URL = (registNumber : number) => `${SERVER_NOTICE_MODULE_URL}/list/${registNumber}`;
+export const INCREASE_NOTICE_VIEW_COUNT_URL = (registNumber : number ) => `${SERVER_API_URL}/${registNumber}/increase-view-count`;
 
+export const REGIST_NOTICE_URL = `${SERVER_NOTICE_MODULE_URL}/regist`;
+export const REGIST_NOTICE_DETAIL_URL = (registNumber : number) => `${SERVER_NOTICE_MODULE_URL}/list/${registNumber}`;
 
-// description : Navigation URL PATH 
-// export const SNS_PATH = '/sns/:accessToken/:expires';
-// export const AUTH_PATH = '/authentication';
-// export const SERVICE_PATH = '/service';
-// export const LOCAL_PATH = 'local';
-// export const RATIO_PATH = 'ratio';
-// export const QNA_PATH = 'qna';
-// export const QNA_WRITE_PATH = 'write';
-// export const QNA_DEATIL_PATH = ':receptionNumber';
-// export const QNA_UPDATE_PATH = 'update/:receptionNumber';
+// description: Q&A MODULE
+export const SERVER_QNA_MODULE_URL = `${SERVER_API_URL}/qna`;
 
-// // description: Navigation 절대 URL PATH 
-// export const AUTH_ABSOLUTE_PATH = AUTH_PATH;
-// export const LOCAL_ABSOLUTE_PATH = `${SERVICE_PATH}/${LOCAL_PATH}`;
-// export const RATIO_ABSOLUTE_PATH = `${SERVICE_PATH}/${RATIO_PATH}`;
-// export const QNA_LIST_ABSOLUTE_PATH = `${SERVICE_PATH}/${QNA_PATH}`;
-// export const QNA_WRITE_ABSOLUTE_PATH = `${SERVICE_PATH}/${QNA_PATH}/${QNA_WRITE_PATH}`;
-// export const QNA_DEATIL_ABSOLUTE_PATH = (receptionNumber: number | string) => `${SERVICE_PATH}/${QNA_PATH}/${receptionNumber}`;
-// export const QNA_UPDATE_ABSOLUTE_PATH = (receptionNumber: number | string) => `${SERVICE_PATH}/${QNA_PATH}/update/${receptionNumber}`;
+export const GET_USER_QNA_LIST_URL = `${SERVER_QNA_MODULE_URL}/myList`;
+export const GET_USER_QNA_LIST_SEARCH_URL = `${SERVER_QNA_MODULE_URL}/myList/search`;
 
-// // description: API URL PATH 
-// export const SERVER_DOMAIN_URL = 'http://localhost:4000';
-// export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/api/v1`;
-// export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
+export const GET_QNA_LIST_URL = `${SERVER_QNA_MODULE_URL}/list`;
+export const GET_QNA_LIST_SEARCH_URL = `${SERVER_API_URL}/list/search`;
+export const GET_QNA_DETAIL_URL = (receptionNumber : number) => `${SERVER_QNA_MODULE_URL}/list/${receptionNumber}`;
+export const QNA_REGIST_URL = `${SERVER_QNA_MODULE_URL}/regist`;
+export const QNA_REGIST_DETAIL_URL = (receptionNumber : number) => `${SERVER_QNA_MODULE_URL}/regist/${receptionNumber}`;
+export const INCREASE_QNA_VIEW_COUNT_URL = (receptionNumber: number) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/increase-view-count`;
 
-// export const SIGN_IN_REUQEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-in`;
-// export const ID_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/id-check`;
-// export const EMAIL_AUTH_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth`;
-// export const EMAIL_AUTH_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth-check`;
-// export const SIGN_UP_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-up`;
+export const POST_COMMENT_REQUEST_URL = (receptionNumber : number) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/admin-comment`;
 
-// export const SERVER_USER_MODULE_URL = `${SERVER_API_URL}/user`;
+// description: COMPANY MODULE
+export const SERVER_COMPANY_MODULE = `${SERVER_API_URL}/company'`;
 
-// export const GET_SIGN_IN_USER_REQUEST_URL = `${SERVER_USER_MODULE_URL}/`;
-
-// export const SERVER_BOARD_MODULE_URL = `${SERVER_API_URL}/board`;
-
-// export const POST_BOARD_REQUEST_URL = `${SERVER_BOARD_MODULE_URL}/`;
-
-// export const POST_COMMENT_REQUEST_URL = (receptionNumber:number | string) => `${SERVER_BOARD_MODULE_URL}/${receptionNumber}/comment`;
-
-// export const GET_BOARD_LIST_URL = `${SERVER_BOARD_MODULE_URL}/list`;
-// export const GET_SEARCH_BOARD_LIST_URL =`${SERVER_BOARD_MODULE_URL}/list/search`;
-// export const GET_BOARD_URL =(receptionNumber: number|string)=> `${SERVER_BOARD_MODULE_URL}/${receptionNumber}`;
-
-// export const INCREASE_VIEW_COUNT_URL = (receptionNumber: number|string)=>`${SERVER_BOARD_MODULE_URL}/${receptionNumber}/increase-view-count`;
-
-// export const DELETE_BOARD_URL = (receptionNumber: number | string) => `${SERVER_BOARD_MODULE_URL}/${receptionNumber}`;
-
-// export const PUT_BOARD_URL = (receptionNumber : number | string) => `${SERVER_BOARD_MODULE_URL}/${receptionNumber}`;
-
-// export const SERVER_ESTATE_MODULE_URL = `${SERVER_API_URL}/estate`;
-
-
-
-
-// // 화살표 함수로 로컬 받아와줌
-// export const GET_LOCAL_DATA_URL =(local : string) => `${SERVER_ESTATE_MODULE_URL}/local/${local}`;
-// export const GET_RATIO_DATA_URL =(local : string)=> `${SERVER_ESTATE_MODULE_URL}/ratio/${local}`;
-
-
-
-// // description : 게시물 상수 
-// export const COUNT_PER_PAGE = 10; 
-// export const COUNT_PER_SECTION = 10; 
+export const GET_COMPANY_LIST_URL = `${SERVER_COMPANY_MODULE}/list`;
+export const GET_COMPANY_LIST_SEARCH_URL = `${SERVER_COMPANY_MODULE}/list/search`;
+export const REGIST_COMPANY_URL = `${SERVER_COMPANY_MODULE}/regist`;
+export const REGIST_COMPANY_DETAIL_URL = (companyCode : number) => `${SERVER_API_URL}/list/${companyCode}`;
