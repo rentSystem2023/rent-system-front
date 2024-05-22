aa361b1f89690c59dc8a26285c1c1c152584d915
qm0u0mp <99dngksmf@gmail.com>
2024-05-22 12:20:36 +0900

    사용자 탑

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { MAIN_PATH } from './constant';
+import ServiceContainer from './layouts/ServiceContainer';
 
 function App() {
-
-  // render
   return (
-  <h1></h1>
-  )
+  <Routes>
+    <Route path={MAIN_PATH} element={<ServiceContainer/>} />  
+  </Routes>);
 }
 
 export default App;
