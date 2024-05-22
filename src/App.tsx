e9a316c0600408fe2d84db5857259cf1ac155f72
qm0u0mp <99dngksmf@gmail.com>
2024-05-22 17:34:39 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { MAIN_PATH, USER_LIST_ABSOLUTE_PATH, USER_LIST_PATH } from './constant';
 import ServiceContainer from './layouts/ServiceContainer';
-import SideContainer from './layouts/SideContainer';
 
 function App() {
   return (
   <Routes>
     <Route path={MAIN_PATH} element={<ServiceContainer/>} />    
-    {/* <Route path={USER_LIST_ABSOLUTE_PATH} element={<SideContainer/>} /> */}
+    {/* <Route path={USER_LIST_ABSOLUTE_PATH} element={<UserContainer/>} /> */}
   </Routes>
   );
 }
