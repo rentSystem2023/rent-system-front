3cb32203c932851230245df8fe89be0bdfe1a337
hhhunnn <https://github.com/hhhunnn>
2024-05-22 15:50:48 +0900

    2024.05.22.front.dto

--- src/apis/qna/dto/request/index.ts
+++ src/apis/qna/dto/request/index.ts
@@ -1,14 +1,22 @@
-// description: Q & A  작성 Request Body DTO //
-export interface PostQnaRequestDto {
+// description: Q&A 게시물 작성하기 Request Body DTO //
+export interface PostQnaBoardRequestDto {
     title: string;
     contents: string;
+    category: string;
+    imageUrl: string;
+    publicState: boolean;
 }
 
+// description: Q&A 게시물 답글 작성하기 Request Body DTO //
 export interface PostQnaRequestDto {
     comment : string;
 }
 
+// description: Q&A 게시물 수정하기 Request Body DTO //
 export interface PutQnaRequestDto {
     title: string;
     contents: string;
+    category: string;
+    imageUrl: string;
+    publicState: boolean;
 }
