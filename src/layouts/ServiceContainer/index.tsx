19be5886a57ba339927c60e0d4ce5375bb9bd086
qm0u0mp <99dngksmf@gmail.com>
2024-05-23 10:37:48 +0900

    사이드바 작업중

--- src/layouts/ServiceContainer/index.tsx
+++ src/layouts/ServiceContainer/index.tsx
@@ -1,48 +1,31 @@
+import { Outlet } from "react-router-dom";
 import "./style.css";
 
-// 로그인 하지 않았을 시
-function TopBar(){
-    return(
-        <div id="wrapper">
-            <div className="logo-container">로고</div>
-            <div className="top-bar-container">
-                <div className="top-bar-button">
-                    <div className="board-button">공지사항</div>            
-                    <div className="board-button">문의사항</div>
-                </div>
-                <div className="top-bar-button">
-                    <div className="board-button">로그인</div>
-                    <div className="board-button">회원가입</div>
-                </div>
+function TopBar() {
+    return (
+        <>
+        <div className="logo-container">로고</div>
+        <div className="top-bar-container">
+            <div className="top-bar-button">
+            <div className="board-button">공지사항</div>
+            <div className="board-button">문의사항</div>
             </div>
-            <div className="main-container">
-                <div className="main-container">메인</div>
+            <div className="top-bar-button">
+            <div className="board-button">로그인</div>
+            <div className="board-button">회원가입</div>
             </div>
         </div>
+        </>
     );
-}
+    }
 
-function SideContainer(){
-    
-}
-
-export default function ServiceContainer (){
+    export default function ServiceContainer() {
     return (
         <div id="wrapper">
-            <div className="logo-container">로고</div>
-            <div className="top-bar-container">
-                <div className="top-bar-button">
-                    <div className="board-button">공지사항</div>            
-                    <div className="board-button">문의사항</div>
-                </div>
-                <div className="top-bar-button">
-                    <div className="board-button">로그인</div>
-                    <div className="board-button">회원가입</div>
-                </div>
-            </div>
-            <div className="main-container">
-                <div className="main-container">메인</div>
-            </div>
+        <TopBar />
+        <div className="main-container">
+            <Outlet />
+        </div>
         </div>
     );
-} 
+}
