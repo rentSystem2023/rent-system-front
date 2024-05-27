d09cc519f365014aebc82de62bc663d04cf5d3aa
qm0u0mp <99dngksmf@gmail.com>
2024-05-27 17:53:16 +0900

    app.tsx 수정

--- src/layouts/AdminContainer/index.tsx
+++ src/layouts/AdminContainer/index.tsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import "./style.css";
+import { Outlet } from 'react-router-dom';
 
 function SideBar () {
     return (
-        <>
-            <div id="user-list-wrapper">
-            <div className="side-navigation-container">
-                <div className="side-navigation-item">
-                    <div className="side-navigation-icon person"></div>
-                    <div className="side-navigation-item-title">회원 관리</div>
-                </div>
-                <div className="side-navigation-item">
-                    <div className="side-navigation-icon office"></div>
-                    <div className="side-navigation-item-title">업체 관리</div>
-                </div>
-                <div className="side-navigation-item">
-                    <div className="side-navigation-icon check"></div>
-                    <div className="side-navigation-item-title">예약 관리</div>
-                </div>
-                <div className="side-navigation-item">
-                    <div className="side-navigation-icon board"></div>
-                    <div className="side-navigation-item-title">게시물 관리</div>
-                </div>
+        <div className="side-navigation-container">
+            <div className="side-navigation-item">
+                <div className="side-navigation-icon person"></div>
+                <div className="side-navigation-item-title">회원 관리</div>
             </div>
+            <div className="side-navigation-item">
+                <div className="side-navigation-icon office"></div>
+                <div className="side-navigation-item-title">업체 관리</div>
             </div>
-        </>
+            <div className="side-navigation-item">
+                <div className="side-navigation-icon check"></div>
+                <div className="side-navigation-item-title">예약 관리</div>
+            </div>
+            <div className="side-navigation-item">
+                <div className="side-navigation-icon board"></div>
+                <div className="side-navigation-item-title">게시물 관리</div>
+            </div>
+        </div>
     )
 }
 
 export default function AdminContainer() {
     return (
-        <div>
+        <div id='admin-wrapper'>
             <SideBar/>
+            <div id='admin-main'>
+                <Outlet />
+            </div>
+            
         </div>
     )
 }
