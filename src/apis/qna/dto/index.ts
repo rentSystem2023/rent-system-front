811efaba78ec27b068bb9a0f3dd6429cbf80f3f3
devKMC <allcasa@naver.com>
2024-05-28 17:49:12 +0900

    2024-05-28 kim

--- src/apis/qna/dto/index.ts
+++ src/apis/qna/dto/index.ts
@@ -25,7 +25,7 @@ export const PostCommentRequest = async(receptionNumber: number | string, reques
 
 
 // function : Q&A 전체 리스트 불러오기 API 함수
-export const getBoardListRequest = async(accessToken: string) => { // 토큰을 포함해서 가져와야 함
+export const getQnaListRequest = async(accessToken: string) => { // 토큰을 포함해서 가져와야 함
     const result = await axios.get(GET_QNA_LIST_URL,bearerAuthorization(accessToken))
     .then(requestHandler<GetQnaBoardListResponseDto>) //위에 코드가 성공한다면 <GetBoardListResponseDto> 받을 것이고
     .catch(requestErrorHandler); // 실패한다면 에러
