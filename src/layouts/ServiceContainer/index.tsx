e001f9a0a2ae98e23f2488dfc2b61857f73bef1f
qm0u0mp <99dngksmf@gmail.com>
2024-05-28 10:29:55 +0900

    상단바 기능 추가

--- src/layouts/ServiceContainer/index.tsx
+++ src/layouts/ServiceContainer/index.tsx
@@ -1,19 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import "./style.css";
+import { useEffect, useState } from "react";
+import { MAIN_ABSOLUTE_PATH, NOTICE_LIST_ABSOLUTE_PATH, QNA_LIST_ABSOLUTE_PATH, SIGN_IN_ABSOLUTE_PATH, SIGN_UP_ABSOLUTE_PATH } from "src/constant";
+
+type Path = '로고' | '공지사항' | '문의사항' | '로그인' | '회원가입' | '';
+
+interface Props {
+    path: Path;
+}
 
 // TODO: 로그인, 회원가입에 아이콘 넣어야함
 function TopBar() {
+
+    const navigator = useNavigate();
+    const { pathname } = useLocation();
+
+    const onLogoClickHandler = () => {
+        if(pathname === MAIN_ABSOLUTE_PATH){
+            window.location.reload();
+        } else {
+        navigator(MAIN_ABSOLUTE_PATH);}
+    }
+        
+    const onNoticeClickHandler = () => navigator(NOTICE_LIST_ABSOLUTE_PATH);
+    const onQnaClickHandler = () => navigator(QNA_LIST_ABSOLUTE_PATH);
+    const onSignInClickHandler = () => navigator(SIGN_IN_ABSOLUTE_PATH);
+    const onSignUpClickHandler = () => navigator(SIGN_UP_ABSOLUTE_PATH);
+
     return (
         <>
-        <div className="logo-container">로고</div>
+        <div className="logo-container" onClick={onLogoClickHandler}>로고</div>
         <div className="top-bar-container">
             <div className="top-bar-button">
-            <div className="board-button">공지사항</div>
-            <div className="board-button">문의사항</div>
+            <div className="top-button" onClick={onNoticeClickHandler}>공지사항</div>
+            <div className="top-button" onClick={onQnaClickHandler}>문의사항</div>
             </div>
             <div className="top-bar-button">
-            <div className="board-button">로그인</div>
-            <div className="board-button">회원가입</div>
+            <div className="top-button" onClick={onSignInClickHandler}>로그인</div>
+            <div className="top-button" onClick={onSignUpClickHandler}>회원가입</div>
             </div>
         </div>
         </>
@@ -32,9 +56,24 @@ function BottomBar() {
 }
 
     export default function ServiceContainer() {
+
+    const { pathname } = useLocation();
+    const [path, setPath] = useState<Path>('');
+
+    useEffect(() => {
+        const path = 
+            pathname === MAIN_ABSOLUTE_PATH ? '로고' :
+            pathname === NOTICE_LIST_ABSOLUTE_PATH ? '공지사항' :
+            pathname === QNA_LIST_ABSOLUTE_PATH ? '문의사항' :
+            pathname === SIGN_IN_ABSOLUTE_PATH ? '로그인' :
+            pathname === SIGN_UP_ABSOLUTE_PATH ? '회원가입' : '';
+
+        setPath(path);
+    }, [pathname]);
+
     return (
         <div id="wrapper">
-        <TopBar/>
+        <TopBar />
         <div className="main-container">
             <Outlet />
         </div>
