22996a03461454d7f15667a1418d12cc117ed090
qm0u0mp <99dngksmf@gmail.com>
2024-05-28 12:23:02 +0900

    URL Constant Modified!

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import {
-  ADMIN_BOARD_NOTICE_ABSOLUTE_PATH,
   ADMIN_BOARD_NOTICE_PATH,
+  ADMIN_BOARD_NOTICE_REGIST_PATH,
   ADMIN_BOARD_NOTICE_UPDATE_PATH,
-  ADMIN_BOARD_NOTICE_WRITE_PATH,
   ADMIN_BOARD_PATH,
   ADMIN_BOARD_QNA_PATH,
   ADMIN_COMPANY_PATH,
   ADMIN_COMPANY_REGIST_PATH,
   ADMIN_COMPANY_UPDATE_PATH,
+  ADMIN_NOTICE_LIST_ABSOLUTE_PATH,
   ADMIN_PATH,
   ADMIN_RESERVATION_PATH,
   ADMIN_RESERVATION_UPDATE_PATH,
   ADMIN_USER_DETAIL_PATH,
   ADMIN_USER_LIST_ABSOLUTE_PATH,
   ADMIN_USER_PATH,
+  AUTH_FIND_ID_PATH,
+  AUTH_FIND_PASSWORD_PATH,
   AUTH_PATH,
+  AUTH_SIGN_IN_PATH,
+  AUTH_SIGN_UP_PATH,
   MAIN_ABSOLUTE_PATH,
   MAIN_PATH,
   NOTICE_DETAIL_PATH,
@@ -24,14 +28,17 @@ import {
   NOTICE_PATH,
   QNA_DETAIL_PATH,
   QNA_LIST_PATH,
-  QNA_MY_LIST_PATH,
   QNA_PATH,
   QNA_REGIST_PATH,
   QNA_UPDATE_PATH,
-  SIGN_IN_PATH,
-  SIGN_UP_PATH,
-  USER_INFO_PATH,
+  RESERVATION_CAR_ABSOLUTE_PATH,
+  RESERVATION_CAR_PATH,
+  RESERVATION_COMPANY_PATH,
+  RESERVATION_PATH,
+  RESERVATION_REQUEST_PATH,
   USER_PATH,
+  USER_QNA_PATH,
+  USER_RESERVATION_PATH,
   USER_UPDATE_PATH,
 } from "./constant";
 import ServiceContainer from "./layouts/ServiceContainer";
@@ -47,7 +54,9 @@ import QnaDetail from "./views/Board/qna/QnaDetail";
 import MyInfo from "./views/MyInfo/Information";
 import MyInfoModify from "./views/MyInfo/Modify";
 import AdminContainer from "./layouts/AdminContainer";
+import UserContainer from "./layouts/UserContainer";
 
+//          component:  //
 function Index() {
   // state
   const [cookies] = useCookies();
@@ -69,6 +78,7 @@ function Index() {
   return <></>;
 }
 
+//          component:  //
 function AdminUserIndex() {
 
   // function
@@ -81,17 +91,33 @@ function AdminUserIndex() {
   return <></>;
 }
 
+//          component:  //
 function AdminBoardIndex() {
+
   // function
   const navigator = useNavigate();
 
   // effect
-  useEffect(() => navigator(ADMIN_BOARD_NOTICE_ABSOLUTE_PATH), []);
+  useEffect(() => navigator(ADMIN_NOTICE_LIST_ABSOLUTE_PATH), []);
 
   // render
   return <></>;
 }
 
+//          component: 예약 페이지 인덱스 컴포넌트          //
+function ReservationIndex() {
+
+  // function
+  const navigator = useNavigate();
+
+  // effect
+  useEffect(() => navigator(RESERVATION_CAR_ABSOLUTE_PATH), []);
+
+  // render
+  return <></>;
+}
+
+//          component:  //
 function App() {
   return (
     <Routes>
@@ -103,34 +129,40 @@ function App() {
 
         {/* // route : 인증 페이지 */}
         <Route path={AUTH_PATH}>
-          <Route path={SIGN_IN_PATH} element={<SignIn />} />
-          <Route path={SIGN_UP_PATH} element={<SignUp />} />
+          <Route path={AUTH_SIGN_IN_PATH} element={<SignIn />} />
+          <Route path={AUTH_SIGN_UP_PATH} element={<SignUp />} />
+          <Route path={AUTH_FIND_ID_PATH} element={<>아이디 찾기</>} />
+          <Route path={AUTH_FIND_PASSWORD_PATH} element={<>비밀번호 찾기</>} />
+        </Route>
+
+        {/* // route : 예약 페이지 */}
+        <Route path={RESERVATION_PATH}>
+          <Route index element={<ReservationIndex />} />
+          <Route path={RESERVATION_CAR_PATH} element={<>차량 선택</>} />
+          <Route path={RESERVATION_COMPANY_PATH} element={<>업체 선택</>} />
+          <Route path={RESERVATION_REQUEST_PATH} element={<>예약 신청</>} />
         </Route>
 
         {/* // route : 마이 페이지 */}
-        <Route path={USER_PATH}>
-          <Route path={USER_INFO_PATH}>
-            <Route index element={<MyInfo />} />
-            <Route path={USER_UPDATE_PATH} element={<MyInfoModify />} />
-          </Route>
+        <Route path={USER_PATH} element={<UserContainer />}>
+          <Route index element={<>내 정보</>} />
+          <Route path={USER_UPDATE_PATH} element={<>내 정보 수정</>} />
+          <Route path={USER_RESERVATION_PATH} element={<>예약내역</>} />
+          <Route path={USER_QNA_PATH} element={<>문의내역</>} />
         </Route>
 
         {/* // route : 공지사항 페이지 */}
         <Route path={NOTICE_PATH}>
-          <Route path={NOTICE_LIST}>
-            <Route path={NOTICE_DETAIL_PATH} />
-          </Route>
+          <Route index element={<>공지사항 리스트</>} />
+          <Route path={NOTICE_DETAIL_PATH} element={<>공지사항 상세보기</>}/>
         </Route>
 
         {/* // route : Q&A 페이지 */}
         <Route path={QNA_PATH} element={<QnAContainer />}>
-          <Route path={QNA_LIST_PATH}>
-            <Route index element={<QnaList />} />
-            <Route path={QNA_DETAIL_PATH} element={<QnaDetail />} />
-            <Route path={QNA_UPDATE_PATH} />
-          </Route>
-          <Route path={QNA_REGIST_PATH} />
-          <Route path={QNA_MY_LIST_PATH} />
+          <Route index element={<>Q&A 리스트</>} />
+          <Route path={QNA_DETAIL_PATH} element={<>Q&A 상세</>} />
+          <Route path={QNA_REGIST_PATH} element={<>Q&A 작성</>} />
+          <Route path={QNA_UPDATE_PATH} element={<>Q&A 수정</>} />
         </Route>
 
         {/* // route : 관리자 페이지 */}
@@ -161,7 +193,7 @@ function App() {
             {/* // route : 관리자 - 게시물 (공지사항) 관리 페이지 */}
             <Route path={ADMIN_BOARD_NOTICE_PATH}>
               <Route index element={<>공지사항 관리 리스트 페이지</>} />
-              <Route path={ADMIN_BOARD_NOTICE_WRITE_PATH} element={<>공지사항 작성 페이지</>} />
+              <Route path={ADMIN_BOARD_NOTICE_REGIST_PATH} element={<>공지사항 작성 페이지</>} />
               <Route path={ADMIN_BOARD_NOTICE_UPDATE_PATH} element={<>공지사항 수정 페이지</>} />
             </Route>
 
