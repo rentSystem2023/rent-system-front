873239490dc88161a4550fb67d73451c5a2d89ad
qm0u0mp <99dngksmf@gmail.com>
2024-05-29 15:05:13 +0900

    레이아웃 끝

--- src/layouts/ServiceContainer/index.tsx
+++ src/layouts/ServiceContainer/index.tsx
@@ -4,17 +4,34 @@ import { useEffect, useState } from "react";
 import { ADMIN_USER_LIST_ABSOLUTE_PATH, AUTH_SIGN_IN_ABSOLUTE_PATH, AUTH_SIGN_UP_ABSOLUTE_PATH, MAIN_ABSOLUTE_PATH, NOTICE_LIST_ABSOLUTE_PATH, QNA_LIST_ABSOLUTE_PATH, USER_INFO_ABSOLUTE_PATH } from "src/constant";
 import { useUserStore } from "src/stores";
 import { useCookies } from "react-cookie";
-import { GetSignInUserResponseDto } from "src/apis/user/dto/response";
+import { GetMyInfoResponseDto, GetSignInUserResponseDto } from "src/apis/user/dto/response";
 import ResponseDto from "src/apis/response.dto";
-import { getSignInUserRequest } from "src/apis/user";
+import { getMyInfoRequest, getSignInUserRequest } from "src/apis/user";
 
 // TODO: 로그인, 회원가입에 아이콘 넣어야함
 function TopBar() {
 
+    const [nickName, setNickName] = useState<string>('');
+
     const navigator = useNavigate();
-    const [cookies, setCookie, removeCookie] = useCookies();
+    const [cookies, removeCookie] = useCookies();
     const { pathname } = useLocation();
     const { loginUserRole } = useUserStore();
+
+    const getMyInfoResponse = (result: GetMyInfoResponseDto | ResponseDto | null) => {
+
+        if (!result) return;        
+
+        const { nickName } = result as GetMyInfoResponseDto;
+        setNickName(nickName);
+
+    };
+    
+    useEffect (() => {
+        if (!cookies.accessToken) return;
+
+        getMyInfoRequest(cookies.accessToken).then(getMyInfoResponse);
+    }, [cookies.accessToken]);
 
     // 로그아웃 처리 시 원래 있던 쿠키 값을 제거
     const onLogoutClickHandler = () => {
@@ -48,7 +65,7 @@ function TopBar() {
             <div className="top-bar-role">
                 <div className="sign-in-wrapper">
                     <div className="user-mypage-button person"></div>
-                    <div className="user-button" onClick={onMyPageClickHandler}>{}</div>
+                    <div className="user-button" onClick={onMyPageClickHandler}>{nickName}님</div>
                 </div>
                 <div className="logout-button" onClick={onLogoutClickHandler}>로그아웃</div>
                 
@@ -94,20 +111,10 @@ function BottomBar() {
     
     const getSignInUserResponse = (result: GetSignInUserResponseDto | ResponseDto | null) => {
 
-        const message = 
-            !result ? '서버에 문제가 있습니다.' :
-            result.code === 'AF' ? '인증에 실패했습니다.' :
-            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
-
-        if (!result || result.code !== 'SU') {
-            alert(message);
-            navigator(MAIN_ABSOLUTE_PATH);
-            return;
-        }
-
         const { userId, userRole } = result as GetSignInUserResponseDto;
         setLoginUserId(userId);
         setLoginUserRole(userRole);
+        
     };
 
     useEffect(() => {
