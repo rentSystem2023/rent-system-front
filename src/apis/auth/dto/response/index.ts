425dcd69b36d6593d2192204db1bd63ee5e52e0a
hhhunnn <gusdk2090@naver.com>
2024-05-31 11:03:56 +0900

    아이디 찾기

--- src/apis/auth/dto/response/index.ts
+++ src/apis/auth/dto/response/index.ts
@@ -5,3 +5,8 @@ export interface SignInResponseDto extends ResponseDto {
     accessToken: string;
     expires: number;
 }
+
+// description: 아이디 찾기 Response Body DTO
+export interface FindIdResponseDto extends ResponseDto {
+    userId: string;
+}
