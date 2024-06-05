a81ab01b548e7471a8e6566fe828b32292b2f209
qm0u0mp <99dngksmf@gmail.com>
2024-06-05 14:46:09 +0900

    예약 페이지 css 작업중

--- src/layouts/AdminContainer/index.tsx
+++ src/layouts/AdminContainer/index.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { ADMIN_COMPANY_LIST_ABSOLUTE_PATH, ADMIN_NOTICE_LIST_ABSOLUTE_PATH, ADMIN_QNA_LIST_ABSOLUTE_PATH, ADMIN_RESERVATION_LIST_ABSOLUTE_PATH, ADMIN_USER_LIST_ABSOLUTE_PATH } from 'src/constant';
 
-type Path = '회원 관리' | '업체 관리' | '예약 관리' | '게시판 관리' | '공지사항 관리' | 'Q&A 관리' | ''; 
+type Path = '회원 관리' | '업체 관리' | '예약 관리' | '' ;
 interface Props {
     path: Path;
 }
@@ -15,9 +15,9 @@ function SideBar ({ path } : Props) {
     const userClass = `admin-side-navigation-item${path === '회원 관리' ? ' active' : ''}`;
     const companyClass = `admin-side-navigation-item${path === '업체 관리' ? ' active' : ''}`;
     const reservationClass = `admin-side-navigation-item${path === '예약 관리' ? ' active' : ''}`;
-    const boardClass = `admin-side-navigation-item${path === '게시판 관리' || path === '공지사항 관리' || path === 'Q&A 관리' ? ' active' : ''}`;
-    const noticeBoardClass = `admin-side-navigation-sub-item${path === '공지사항 관리' ? ' active' : ''}`;
-    const qnaBoardClass = `admin-side-navigation-sub-item${path === 'Q&A 관리' ? ' active' : ''}`;
+    // const boardClass = `admin-side-navigation-item${path === '게시판 관리' || path === '공지사항 관리' || path === 'Q&A 관리' ? ' active' : ''}`;
+    // const noticeBoardClass = `admin-side-navigation-sub-item${path === '공지사항 관리' ? ' active' : ''}`;
+    // const qnaBoardClass = `admin-side-navigation-sub-item${path === 'Q&A 관리' ? ' active' : ''}`;
 
     const navigator = useNavigate();
 
@@ -45,7 +45,7 @@ function SideBar ({ path } : Props) {
                 <div className="admin-side-navigation-icon check"></div>
                 <div className="admin-side-navigation-item-title">예약 관리</div>
             </div>
-            <div className={boardClass} onClick={onBoardClickHandler}>
+            {/* <div className={boardClass} onClick={onBoardClickHandler}>
                 <div className="admin-side-navigation-icon board"></div>
                 <div className="admin-side-navigation-item-title">게시판 관리</div>
             </div>
@@ -58,7 +58,7 @@ function SideBar ({ path } : Props) {
                     <div className="admin-side-navigation-sub-item-title">Q&A 관리</div>
                 </div>
             </div>
-            )}
+            )} */}
         </div>
     )
 }
@@ -71,10 +71,10 @@ export default function AdminContainer() {
     useEffect(() => {
         const path = 
             pathname === ADMIN_USER_LIST_ABSOLUTE_PATH ? '회원 관리' :
-            pathname === ADMIN_COMPANY_LIST_ABSOLUTE_PATH ? '업체 관리' :
-            pathname === ADMIN_RESERVATION_LIST_ABSOLUTE_PATH ? '예약 관리' : 
-            pathname === ADMIN_NOTICE_LIST_ABSOLUTE_PATH ? '공지사항 관리' : 
-            pathname === ADMIN_QNA_LIST_ABSOLUTE_PATH ? 'Q&A 관리' : '';
+            pathname === ADMIN_COMPANY_LIST_ABSOLUTE_PATH ? '업체 관리' : '' ;
+            // pathname === ADMIN_RESERVATION_LIST_ABSOLUTE_PATH ? '예약 관리' : 
+            // pathname === ADMIN_NOTICE_LIST_ABSOLUTE_PATH ? '공지사항 관리' : 
+            // pathname === ADMIN_QNA_LIST_ABSOLUTE_PATH ? 'Q&A 관리' : '';
     
         setPath(path);
     }, [pathname]);
