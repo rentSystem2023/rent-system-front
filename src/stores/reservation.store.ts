de4af83337cf7e07ad29b21477691b99eea0a49c
qm0u0mp <99dngksmf@gmail.com>
2024-06-12 15:47:13 +0900

    company list 구현 완료

--- src/stores/reservation.store.ts
+++ src/stores/reservation.store.ts
@@ -1,3 +1,4 @@
+import { ReservationCarViewListItem } from 'src/types';
 import { getYYYYMMDD } from 'src/utils';
 import { create } from 'zustand';
 
@@ -5,18 +6,26 @@ interface ReservationStore {
     address: string;
     reservationStart: string;
     reservationEnd: string;
+    selectedCar: ReservationCarViewListItem | null;
+    selectedInsurance: string;
     setAddress: (address: string) => void;
     setReservationStart: (reservationStart: string) => void;
     setReservationEnd: (reservationEnd: string) => void;
+    setSelectedCar: (selectedCar: ReservationCarViewListItem) => void;
+    setSelectedInsurance: (selectedInsurance: string) => void;
 };
 
 const useReservationStore = create<ReservationStore>(set => ({
     address: '제주국제공항',
     reservationStart: getYYYYMMDD(new Date()),
     reservationEnd: getYYYYMMDD(new Date()),
+    selectedCar: null,
+    selectedInsurance: '',
     setAddress: (address: string) => set(state => ({ ...state, address })),
     setReservationStart: (reservationStart: string) => set(state => ({ ...state, reservationStart })),
     setReservationEnd: (reservationEnd: string) => set(state => ({ ...state, reservationEnd })),
+    setSelectedCar: (selectedCar: ReservationCarViewListItem) => set(state => ({ ...state, selectedCar })),
+    setSelectedInsurance: (selectedInsurance: string) => set(state => ({ ...state, selectedInsurance })),
 }));
 
 export default useReservationStore;
